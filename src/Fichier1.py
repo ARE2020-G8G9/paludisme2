e367df95128dd063a39509d358dfb2fa203e7c84
Thomas Ruan <thomas_ruan@hotmail.fr>
2020-05-05 16:21:34 +0200

    FichiersPython

---
+++ src/Fichier1.py
@@ -0,0 +1,15 @@
+import random
+import tkinter as Tk
+
+# World dimensions
+width=100
+height=100
+canvas_w=1000
+canvas_h=1000
+
+# Race arrays
+race_array = [[0] * height for x in range(width)]
+tk_array = [[None] * height for x in range(width)]
+empty_spaces = []
+
+print("lalalala")
