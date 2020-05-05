81e4c201af22f443ad0c68769d8d27c24208cf6f
Amelie <sun_amelie@hotmail.com>
2020-05-05 18:09:57 +0200

    ajout de la fonction natalite_moustique

--- src/natalite_moustique.py.py
+++
@@ -1,9 +0,0 @@
-def natalite_moustique (nbMoustiques,j):
-#12,3 larves/moustique/jour
-#environ 20 jours pour passer de larve à adulte
-    totM=nbMoustiques
-    for jour in range (1,j):
-        if (jour%20==0):
-            totM=totM+totM*12,3
-        i=i+1
-    return totM
